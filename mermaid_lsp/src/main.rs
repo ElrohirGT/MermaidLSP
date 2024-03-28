6fc029eeeed57f0b9b3637c928b6ea96c90aaaf8
ElrohirGT <elrohirgt@gmail.com>
2024-03-28 17:38:15 -0600

    feat: Added logging

--- mermaid_lsp/src/main.rs
+++ mermaid_lsp/src/main.rs
@@ -1,3 +1,14 @@
+use log::info;
+use simplelog::*;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    WriteLogger::init(
+        LevelFilter::Info,
+        Config::default(),
+        File::create("/home/elrohirgt/Documents/Development/MermaidLSP/mermaid_lsp.log").unwrap(),
+    )
+    .unwrap();
+
+    info!("Logging setup correctly!");
 }
